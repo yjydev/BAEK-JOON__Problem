1e122885f15094f67dd85674aff1668599b7e680
jyjdev <yun22287@naver.com>
2021-09-01 23:18:22 +0900

    modify b1388

--- Aug/dfs & bfs/b1388.py
+++ Aug/dfs & bfs/b1388.py
@@ -3,14 +3,15 @@ N, M = map(int,input().split())
 floor = [list(input()) for _ in range(N)]
 result = 0
 
+# | 나 - 가 처음 나오면 +1, 이전 문자열 갱신하면서, - 인데 이전 문자열이 | 이거나, 반대인 경우 찾으면 + 1 (즉, 연속이 깨지면 +1)
 # - 탐색 (행)
 for i in range(N):
-    previous = '/'
+    previous = '/'      # 이전 문자열 초기화
     for j in range(M):
         if floor[i][j] == '-':
-            if floor[i][j] != previous:
+            if floor[i][j] != previous:     # 이전 문자열과 다르다면 개수 + 1
                 result += 1
-        previous = floor[i][j]
+        previous = floor[i][j]      # 이전 문자열 갱신
 
 # | 탐색 (열)
 for i in range(M):
