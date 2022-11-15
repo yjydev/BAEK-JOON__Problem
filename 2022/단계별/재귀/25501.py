f4c74634febd1858c20aaff51b9b5cb57c7a0410
unknown <yun22287@naver.com>
2022-11-15 17:24:51 +0900

    ADD 단계별_재귀

---
+++ 2022/단계별/재귀/25501.py
@@ -0,0 +1,16 @@
+#재귀의 귀재
+T = int(input())
+def recursion(s, l, r):
+    global cnt
+    cnt += 1
+    if l >= r: return 1
+    elif s[l] != s[r]: return 0
+    else: return recursion(s, l+1, r-1)
+
+def isPalindrome(s) :
+    return recursion(s,0,len(s)-1)
+
+for _ in range(T):
+    S = input()
+    cnt = 0
+    print(isPalindrome(S), cnt)
