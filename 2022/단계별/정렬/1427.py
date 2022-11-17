bde30ffb70f3648bc98a06b0078b7ff725b267e3
unknown <yun22287@naver.com>
2022-11-17 23:34:04 +0900

    ADD 단계별_정렬_1427.py_설명

--- 2022/단계별/정렬/1427.py
+++ 2022/단계별/정렬/1427.py
@@ -1,4 +1,5 @@
 #소트인사이드
-N = list(input())
-N.sort(reverse=True)
+N = list(input()) # 굳이 int 형변환 필요 x => 나중에 join 쓰려면 문자열이어야 하므로
+                # => python은 암시적 형변환을 해서 list 원소가 str형이어도 sort 가능
+N.sort(reverse=True) # 내림차순 정렬
 print(''.join(N))
